c904e1d232e0ce40cb24a134f80eeb774fa358a5
yanbo <2695388808@qq.com>
2020-09-16 23:21:20 +0800

    vue

---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-checkbox class="loginRemeber" v-model="checked"></el-checkbox>
+            <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return {
+                loginForm: {
+                    username:'admin',
+                    password:'123',
+                },
+                checked: true,
+                rules: {            //校验规则定义
+                    username:[{required:true,message:"请输入用户名",trigger:"blur"}],
+                    password:[{required:true,message:"请输入用户名",trigger:"blur"}]
+                }
+            }
+        },
+        methods:{
+            submitLogin() {     //校验成功后执行
+                // alert('submitLogin');
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('请输入正确有效字段');
+                        return false;
+                    }
+                });
+            }
+        },
+    }
+</script>
+
+<style>
+    .loginContainer{
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 20px 25px 15px 35px;
+        background: aliceblue;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+    .loginTitle{
+        margin: 10px auto 40px 20px;
+        text-align: center;
+        color: black;
+    }
+    .loginRemeber{
+        text-align: left;
+        margin: 0px 0px 20px 0px;
+    }
+</style>
